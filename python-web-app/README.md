294cac4fc8d4fc3fdc1cb42491d57ab6fc3614c9
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2024-11-03 13:13:26 +0530

    Updating urls file

--- python-web-app/README.md
+++ python-web-app/README.md
@@ -1,7 +1,17 @@
 This project includes all the docker related learnings with examples.
 
-Comands:
+Commands:
+
+- Update the existing packages and install docker.
+
+- sudo apt update && sudo apt-get install docker.io -y
 
 - Create django project : python -m django startproject devops
 
 - python manage.py startapp demo (need to run this command in the same folder as manage.py file)
+
+- docker build .
+
+- docker images
+
+- docker run -p 8000:8000 -it <image id>
--- python-web-app/devops/devops/urls.py
+++ python-web-app/devops/devops/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for devops project.
+"""devops URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/demo/', permanent=True)),
 ]
